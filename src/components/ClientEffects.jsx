6cb5f197102223d41324e8fd353a1eedb646c18f
Brageanth David Palencia <brageanth@gmail.com>
2025-07-02 23:28:34 -0500

    fix: gsap

--- src/components/ClientEffects.jsx
+++ src/components/ClientEffects.jsx
@@ -11,12 +11,13 @@ export default function ScrollAndLenis() {
       gsap.from(element, {
         scrollTrigger: {
           trigger: element,
-          start: "top 90%",
-          opacity: 0,
-          y: 50,
-          duration: 1.2,
-          ease: "power2.out",
+          start: "top 90%", // cuando el top del elemento llega al 90% del viewport
+          toggleActions: "play none none none", // solo anima una vez
         },
+        opacity: 0,
+        y: 50,
+        duration: 1.2,
+        ease: "power2.out",
       });
     });
 
