7acd4bd6bbb6c31444b123f9bef890a4376832d5
Brageanth David Palencia <brageanth@gmail.com>
2025-07-02 22:52:57 -0500

    feat: add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://brageanth.github.io",
-  base: "rema_travel",
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
