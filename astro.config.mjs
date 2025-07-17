d6dee7f65e667a86a96d57718a3eaf576bf5ea4c
Brageanth David Palencia <brageanth@gmail.com>
2025-07-17 12:47:40 -0500

    feat: indexing improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,14 @@ import vercel from "@astrojs/vercel";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
+  site: "https://rema-travel.vercel.app/",
   adapter: vercel(),
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
